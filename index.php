e88e331187845b7d20d693952717c50777f1aef2
Shawon <sashawon60@gmail.com>
2021-09-11 14:22:37 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<!--Titel-->
+		<title>Networking Projects</title>
+		<link rel="icon" type="image/jpg" sizes="32x32" href="image/shawon.jpg">
+		<!--bootstrap-->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!--JQuery fancybox-->
+		<link rel="stylesheet" href="css/jquery.fancybox.min.css">
+	</head>
+	<body>
+		<!-- ======= Gallery Start ======= -->
+		<section class="gallery bg-light pt-5">
+			<div class="text-center">
+				<h1 class="display-4">Networking Project</h1>
+				<hr class="w-25 mx-auto">
+			</div>
+			<div class="container-flude">
+				<div class="row gy-3 p-5 gallerys">
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+	        			<span>0. LAN Without routing protocol</span>
+						<a href="image/0. LAN Without routing protocol.png" data-fancybox="gallery" data-caption="0. LAN Without routing protocol">
+							<img class="img-fluid about_img" src="image/0. LAN Without routing protocol.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>1. Static ip addressing</span>
+						<a href="image/1. Static ip addressing.png" data-fancybox="gallery" data-caption="1. Static ip addressing">
+							<img class="img-fluid about_img" src="image/1. Static ip addressing.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>2. DHCP</span>
+						<a href="image/2. DHCP.png" data-fancybox="gallery" data-caption="2. DHCP">
+							<img class="img-fluid about_img" src="image/2. DHCP.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>3. Default routing protocol</span>
+						<a href="image/3. Default routing protocol.png" data-fancybox="gallery" data-caption="3. Default routing protocol">
+							<img class="img-fluid about_img" src="image/3. Default routing protocol.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>4. Static routing protocol</span>
+						<a href="image/4. Static routing protocol.png" data-fancybox="gallery" data-caption="4. Static routing protocol">
+							<img class="img-fluid about_img" src="image/4. Static routing protocol.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>5. RIP (dynamic) routing protocol</span>
+						<a href="image/5. RIP (dynamic) routing protocol.png" data-fancybox="gallery" data-caption="5. RIP (dynamic) routing protocol">
+							<img class="img-fluid about_img" src="image/5. RIP (dynamic) routing protocol.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>6. VLSM</span>
+						<a href="image/6. VLSM.png" data-fancybox="gallery" data-caption="6. VLSM">
+							<img class="img-fluid about_img" src="image/6. VLSM.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>7. VLAN & TRUNK</span>
+						<a href="image/7. VLAN & TRUNK.png" data-fancybox="gallery" data-caption="7. VLAN & TRUNK">
+							<img class="img-fluid about_img" src="image/7. VLAN & TRUNK.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>8. Inter VLAN</span>
+						<a href="image/8. Inter VLAN.png" data-fancybox="gallery" data-caption="8. Inter VLAN">
+							<img class="img-fluid about_img" src="image/8. Inter VLAN.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>9. Inter VLAN connection with another network</span>
+						<a href="image/9. Inter VLAN connection with another network.png" data-fancybox="gallery" data-caption="9. Inter VLAN connection with another network">
+							<img class="img-fluid about_img" src="image/9. Inter VLAN connection with another network.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>10. Single area ospf</span>
+						<a href="image/10. Single area ospf.png" data-fancybox="gallery" data-caption="10. Single area ospf">
+							<img class="img-fluid about_img" src="image/10. Single area ospf.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>11. multi area ospf</span>
+						<a href="image/11. multi area ospf.png" data-fancybox="gallery" data-caption="11. multi area ospf">
+							<img class="img-fluid about_img" src="image/11. multi area ospf.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>12. eigrp</span>
+						<a href="image/12. eigrp.png" data-fancybox="gallery" data-caption="12. eigrp">
+							<img class="img-fluid about_img" src="image/12. eigrp.png " alt="images">	
+						</a>
+					</div>
+	        		<div class="col-lg-4 col-md-4 col-12 col-xl-4 col-xxl-4 mx-auto shadow-sm">
+						<span>13. Router Redistribute</span>
+						<a href="image/13. Router Redistribute.png" data-fancybox="gallery" data-caption="13. Router Redistribute">
+							<img class="img-fluid about_img" src="image/13. Router Redistribute.png " alt="images">	
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Gallery -->
+
+		<!--javascript--> 
+		<script src="js/bootstrap.bundle.min.js"></script>
+		<script src="js/jquery-3.5.1.min.js"></script>
+		<script src="js/jquery.fancybox.min.js"></script>
+		<script src="js/smooth-scroll.polyfills.min.js"></script>
+
+		<script>
+			$("[data-fancybox]").fancybox();
+		</script>
+
+		<script>
+			var scroll = new SmoothScroll('a[href*="#"]');
+		</script>
+	</body>
+</html>
